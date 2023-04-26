5fe921c9afbb5a08246078ca13d4bbfc46f5320d
Sbstyn <sebestyen.marton3@gmail.com>
2023-04-26 17:23:00 +0200

    fix

--- up2pico.py
+++ up2pico.py
@@ -3,6 +3,22 @@ import os
 folder = "Prototype_keyb/final"
 
 try:
+    out = os.popen("rshell ls /pyboard/").read()
+    print("______", out, "________________")
+    li = out.split()
+
+    elements = []
+    for e in li:
+        if e[-3:] == ".py" or e[-4:] == ".txt":
+            elements.append(e)
+
+    for e in elements:
+        os.system(f"rshell rm /pyboard/{e}")
+
+except Exception as e:
+    print("failed:", e)
+
+try:
     #os.system("rshell")
     for e in os.listdir(folder):
         os.system(f"rshell cp {folder}/{e} /pyboard/")
